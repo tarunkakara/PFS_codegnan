4c21281f03e70bd892a9e65892ce1f1d6ae7f5b2
tarunkakara <tarunkakara@gmail.com>
2026-05-13 15:36:14 +0530

    Add files via upload
    Completed Day 1 of my Python 100 Days Training at Codegnan
     by learning the basics of Python programming and its real-world applications.
    Learned core concepts including variables, tokens, and Boolean data types with simple practice programs.
    Added beginner-level examples and notes to strengthen programming fundamentals and logic-building skills.

---
+++ Day1.py
@@ -0,0 +1,32 @@
+print("This is my first python class")
+'''
+What is python?
+High level language - No need to worry about low-level management
+Dynamically type language - no need to mention the kind of data we are taking to variable.
+Interpretor language - python excuete line by line and through a error if any error dtected and before lines all are excueted.
+
+-----------------------
+
+What is Tokens
+
+tokens is nothing but "Keywords"
+EX --
+if
+else
+while
+for
+----------------------
+
+Identifiers--
+name of variables, functions and classes
+------------------------------
+
+What is variables?
+----
+Good ways:
+A-Z,a-z, _
+
+Bad Ways:
+special characters, numbers(can be used after the firt letter)
+
+'''
